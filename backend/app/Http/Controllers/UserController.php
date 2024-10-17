cdc5db03b7336b89d0b258a33f65cd5eace82536
Led <procomputers@PROs-MacBook.local>
2024-10-17 14:29:14 +0200

    Create UserController with methods for login, registration and define routes for registration, login and user

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {
+
+    }
+
+    public function login(Request $request) {
+
+    }
+
+    public function user(Request $request) {
+
+
+    }
+}
