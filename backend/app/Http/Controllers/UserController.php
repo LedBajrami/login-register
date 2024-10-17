0d98ab4282525f3c8fa98e4b8dc2823352349398
Led <procomputers@PROs-MacBook.local>
2024-10-17 16:14:10 +0200

    Implement registration and login logic with user authentication

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,19 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function register(Request $request) {
 
+        $request->validate([
+            'name' => 'required', 
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => "User registred succsefully", 'user' => $user], 201);
     }
+
+
 
     public function login(Request $request) {
 
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            return response()->json(['user' => $user], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
     }
 
     public function user(Request $request) {
+        $user = Auth::user();
 
+        if ($user) {
+            return response()->json([
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+            ], 200);
+        }
 
+        return response()->json(['error' => 'User not authenticated'], 401);
     }
 }
