a086e0cbc07da259e43db6363015e227f6d0bf78
Led <procomputers@PROs-MacBook.local>
2024-11-28 17:52:34 +0100

    Update User component to fetch and manage user data with redux

--- frontend/src/components/User/User.jsx
+++ frontend/src/components/User/User.jsx
@@ -1,71 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { getUser } from '../../services/userService';
-import { logout } from '../../services/authService';
 import UserPhotoUpload from './UserPhotoUpload';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from '../../redux/slices/userSlice';
+import { logout } from '../../redux/slices/authSlice';
+import { fetchUser } from '../../redux/slices/userThunks';
+import { Spin } from 'antd';
 
 
 
 function User() {
-  const [user, setUser] = useState({
-    name: '', 
-    email: '', 
-    profilePhoto: null,
-  });
-
+  const {loading, name, email, profilePhoto} = useSelector((state) => state.user)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const data = await getUser()
-        setUser({
-          name: data.name,
-          email: data.email,
-          profilePhoto: data.profile_photo || null,
-        });
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-      }
+    const token = localStorage.getItem('access_token');
+    if (token) {
+        dispatch(fetchUser());
     }
-  
-    fetchUserData();
-  }, [navigate]);
+}, [dispatch]);
 
 
   const handleLogout =  async () => {
-    await logout()
+    dispatch(logout())
     navigate('/');
 };
 
 
 
   return (
+   <Spin spinning={loading} size="large" >
     <div style={{ width: '400px', marginInline: 'auto', paddingTop: '50px' }}>
       
       <h2>User Profile</h2>
 
       <div className='user-info-container' style={{display: "flex", justifyContent: "start", alignItems: "center", marginBlock: "20px"}}>
       <div style={{width: "15%"}}>
-          {user.profilePhoto ? (
-            <img src={`${process.env.REACT_APP_BASE_URL}${user.profilePhoto}`} alt="Profile" style={{ width: '100%', borderRadius: "50%", aspectRatio: "1" }} />
+          {profilePhoto ? (
+            <img src={`${process.env.REACT_APP_BASE_URL}${profilePhoto}`} alt="Profile" style={{ width: '100%', borderRadius: "50%", aspectRatio: "1" }} />
           ) : (
             <p>No profile photo uploaded.</p>
          )}
        </div>
        <div style={{paddingLeft: "20px"}}>
-        <div><strong>Name:</strong> {user.name}</div>
-        <div><strong>Email:</strong> {user.email}</div>
+        <div><strong>Name:</strong> {name}</div>
+        <div><strong>Email:</strong> {email}</div>
        </div>
       </div>
 
-      <UserPhotoUpload user={user} setUser={setUser}/>
+      <UserPhotoUpload/>
       
       <div>
         <button onClick={handleLogout}>Logout</button>
       </div>
     </div>
-    
+    </Spin>   
   );
 }
 
