7f212016b32a571a4820dae1a0e6c6fa1633fbd8
Led <procomputers@PROs-MacBook.local>
2024-11-28 17:33:08 +0100

    Add userThunks for fetching user data and uploadig profile photo

---
+++ frontend/src/redux/slices/userThunks.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { apiClient } from "../../helpers/apiClient";
+import { updateProfilePhoto } from "./userSlice";
+
+export const fetchUser = createAsyncThunk('user/fetchUser', 
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await apiClient('/api/user', {
+                method: "GET"
+            })
+            return response
+        } catch (error) {
+            return rejectWithValue(error.message || "Failed to fetch user data")``
+        }
+    }
+)
+
+
+export const uploadProfilePhoto = createAsyncThunk('user/uploadProfilePhoto', 
+    async (file, {rejectWithValue, dispatch}) => {
+        const formData = new FormData()
+        formData.append('profile_photo', file)
+
+
+        try {
+            const response = await apiClient('/api/user/upload', {
+                method: "POST",
+                body: formData
+            })
+            dispatch(updateProfilePhoto(response.profile_photo))
+            return response.profile_photo
+        } catch (error) {
+            return rejectWithValue('Failed to upload profile photo')
+        }
+    }
+)
